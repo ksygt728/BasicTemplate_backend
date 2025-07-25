c233d76e85ca7f86ca9fb97e14333c4baac0f930
ksygt728 <ksygt728@gamil.com>
2025-07-25 23:51:19 +0900

    Fix(InterfaceReqDTO) : 필드 누락된 부분 추가

--- backend/app/src/main/java/com/basic/app/dto/requestDto/InterfaceReqDto.java
+++ backend/app/src/main/java/com/basic/app/dto/requestDto/InterfaceReqDto.java
@@ -1,6 +1,6 @@
 /**
  * @파일명   : InterfaceReqDto.java
- * @설명     : 
+ * @설명     : 인터페이스 정보 요청 데이터 전송 객체
  * @작성자   : 김승연
  * @작성일   : 2025.07.23
  * @변경이력 :
@@ -34,7 +34,17 @@ public class InterfaceReqDto {
   @NotBlank(groups = { CreateGroup.class, UpdateGroup.class }, message = "인터페이스명은 필수입니다.")
   private String ifName; // 인터페이스명
 
-  private String text; // WSDL
+  @NotBlank(groups = { CreateGroup.class, UpdateGroup.class }, message = "인터페이스 유형은 필수입니다.")
+  private String ifType; // 인터페이스 유형
+
+  @NotBlank(groups = { CreateGroup.class, UpdateGroup.class }, message = "인터페이스 URL은 필수입니다.")
+  private String ifUrl; // 인터페이스 URL
+
+  @NotBlank(groups = { CreateGroup.class, UpdateGroup.class }, message = "HTTP 메소드는 필수입니다.")
+  private String ifMethod; // HTTP 메소드
+
+  @NotBlank(groups = { CreateGroup.class, UpdateGroup.class }, message = "사용여부는 필수입니다.")
+  private String useYn; // 사용여부
 
   @NotBlank(groups = { CreateGroup.class }, message = "생성자는 필수입니다.")
   private String createUser; // 생성자
