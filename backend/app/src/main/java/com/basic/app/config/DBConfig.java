689f183c1181f94c6f1bb967d6448cd63a2f529c
ksygt728 <ksygt728@gamil.com>
2025-07-25 23:54:58 +0900

    Feat : Jooq동적쿼리 사용하기 위해 추가

--- backend/app/src/main/java/com/basic/app/config/DBConfig.java
+++ backend/app/src/main/java/com/basic/app/config/DBConfig.java
@@ -1,51 +1,51 @@
-/**
- * @파일명   : DBConfig.java
- * @설명     : 데이터베이스 연결 설정
- * @작성자   : 김승연
- * @작성일   : 2025.07.24
- * @변경이력 :
- *   2025.07.24     김승연       최초 생성
- */
-package com.basic.app.config;
+// /**
+// * @파일명 : DBConfig.java
+// * @설명 : 데이터베이스 연결 설정
+// * @작성자 : 김승연
+// * @작성일 : 2025.07.24
+// * @변경이력 :
+// * 2025.07.24 김승연 최초 생성
+// */
+// package com.basic.app.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+// import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+// import jakarta.persistence.EntityManager;
+// import jakarta.persistence.PersistenceContext;
 
-@Configuration
-public class DBConfig {
+// @Configuration
+// public class DBConfig {
 
-	// queryDSL 설정
-	@PersistenceContext
-	private EntityManager entityManager;
+// // queryDSL 설정
+// @PersistenceContext
+// private EntityManager entityManager;
 
-	@Bean
-	public JPAQueryFactory jpaQueryFactory() {
-		// Provider<EntityManager> entityManager
-		return new JPAQueryFactory(entityManager);
-	}
+// @Bean
+// public JPAQueryFactory jpaQueryFactory() {
+// // Provider<EntityManager> entityManager
+// return new JPAQueryFactory(entityManager);
+// }
 
-	// Spring data JPA -> application.properties에서 설정
+// // Spring data JPA -> application.properties에서 설정
 
-	// JDBC Template 설정
-	// @Bean
-	// public ComboPooledDataSource dataSource() throws PropertyVetoException {
-	// ComboPooledDataSource dataSource = new ComboPooledDataSource();
-	//
-	// dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-	// dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/lins_finaltest");
-	// dataSource.setUser("root");
-	// dataSource.setPassword("jx1019aw!!@@");
-	// dataSource.setMaxPoolSize(200);
-	// dataSource.setCheckoutTimeout(60000);
-	// dataSource.setMaxIdleTime(1800);
-	// dataSource.setIdleConnectionTestPeriod(600);
-	//
-	// return dataSource;
-	//
-	// }
-}
+// // JDBC Template 설정
+// // @Bean
+// // public ComboPooledDataSource dataSource() throws PropertyVetoException {
+// // ComboPooledDataSource dataSource = new ComboPooledDataSource();
+// //
+// // dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+// // dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/lins_finaltest");
+// // dataSource.setUser("root");
+// // dataSource.setPassword("jx1019aw!!@@");
+// // dataSource.setMaxPoolSize(200);
+// // dataSource.setCheckoutTimeout(60000);
+// // dataSource.setMaxIdleTime(1800);
+// // dataSource.setIdleConnectionTestPeriod(600);
+// //
+// // return dataSource;
+// //
+// // }
+// }
