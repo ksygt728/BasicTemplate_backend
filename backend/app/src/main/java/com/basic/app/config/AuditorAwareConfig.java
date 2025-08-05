8c9bb82a55fa08964d1abce093bdc425da514121
ksygt728 <ksygt728@gamil.com>
2025-08-05 18:11:50 +0900

    Chore(Audit) : Sprign Security 적용으로 현재 인증된 유제정보로 자동 세팅하도록 변경

--- backend/app/src/main/java/com/basic/app/config/AuditorAwareConfig.java
+++ backend/app/src/main/java/com/basic/app/config/AuditorAwareConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @EnableJpaAuditing
 @Configuration
@@ -14,15 +16,12 @@ public class AuditorAwareConfig {
   @Bean
   public AuditorAware<String> auditorProvider() {
     return () -> {
-      return Optional.of("SYSTEM_Audit"); // 기본적으로 "SYSTEM"을 반환합니다.
-
       // 실제 로그인 정보에서 가져오기 (예: Spring Security 사용 시)
-      // Authentication authentication =
-      // SecurityContextHolder.getContext().getAuthentication();
-      // if (authentication == null || !authentication.isAuthenticated()) {
-      // return Optional.of("SYSTEM");
-      // }
-      // return Optional.of(authentication.getName());
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication == null || !authentication.isAuthenticated()) {
+        return Optional.of("SYSTEM");
+      }
+      return Optional.of(authentication.getName());
     };
   }
 }
