7ed6a0b5f9fd7c2a6c8cf6521539d631bf4010d9
ksygt728 <ksygt728@gamil.com>
2025-08-05 21:04:06 +0900

    Fix(Audit) : annonymous로 받을 경우 SYSTEM으로 처리되도록 변경

--- backend/app/src/main/java/com/basic/app/config/AuditorAwareConfig.java
+++ backend/app/src/main/java/com/basic/app/config/AuditorAwareConfig.java
@@ -9,8 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import lombok.extern.log4j.Log4j2;
+
 @EnableJpaAuditing
 @Configuration
+@Log4j2
 public class AuditorAwareConfig {
 
   @Bean
@@ -18,7 +21,10 @@ public class AuditorAwareConfig {
     return () -> {
       // 실제 로그인 정보에서 가져오기 (예: Spring Security 사용 시)
       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (authentication == null || !authentication.isAuthenticated()) {
+      log.error("Authentication : " + authentication);
+      log.error("Authentication name : " + authentication.getName());
+      if (authentication == null || !authentication.isAuthenticated()
+          || authentication.getName().equals("anonymousUser")) {
         return Optional.of("SYSTEM");
       }
       return Optional.of(authentication.getName());
