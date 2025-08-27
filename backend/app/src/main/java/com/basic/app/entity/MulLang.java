61baeb2bf93d486b04a10cd95b3ffed7d00dd55b
ksygt728 <ksygt728@gamil.com>
2025-08-27 22:45:54 +0900

    Fix(다국어) : 다국어 Key구조 수정

--- backend/app/src/main/java/com/basic/app/entity/MulLang.java
+++ backend/app/src/main/java/com/basic/app/entity/MulLang.java
@@ -1,10 +1,11 @@
 package com.basic.app.entity;
 
 import com.basic.app.entity.baseEntity.BaseEntity;
+import com.basic.app.entity.compositeKey.MulLangId;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,18 +25,12 @@ import lombok.ToString;
 @Entity
 @Table(name = "TB_MUL_LANG") // 다국어 테이블
 public class MulLang extends BaseEntity {
-  @Id
-  @Column(name = "LANG_CD", length = 45)
-  private String langCd; // 언어코드
 
-  @Column(name = "LANG_TYPE", length = 45, nullable = false)
-  private String langType; // 언어유형
+  @EmbeddedId
+  private MulLangId mulLang; // 복합키: 언어코드, 언어유형
 
   @Column(name = "LANG_NM", length = 2048, nullable = false)
   private String langNm; // 언어명
-
-  @Column(name = "LANG_GUBUN", length = 45, nullable = false)
-  private String langGubun; // 언어구분
 
   @Column(name = "USE_YN", length = 1, nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
   private String useYn; // 사용여부 (Y,N)
